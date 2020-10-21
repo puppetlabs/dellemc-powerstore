09da26039459454644c2ca3f21b4527e08a11fd6
Dimitri Tischenko <dimitri@puppet.com>
2020-10-21 15:06:42 +0200

    Added create_multiple_volumes

---
+++ plans/create_multiple_volumes.pp
@@ -0,0 +1,10 @@
+# This plan creates multiple volumes
+plan powerstore::create_multiple_volumes(
+  TargetSpec $targets,
+  Integer[1,100] $volume_count,
+){
+  Integer[1,$volume_count].each |$i| {
+    out::message("Creating volume vol_${i}")
+    run_task('powerstore::volume_create', $targets, {name=>"vol_${i}", size=>1048576})
+  }
+}
