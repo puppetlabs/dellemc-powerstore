3492a004b95c1a6f7aed09ec6152daee1676cb11
Dimitri Tischenko <dimitri@puppet.com>
2020-10-21 15:43:28 +0200

    apply alternative added

--- plans/create_multiple_volumes.pp
+++ plans/create_multiple_volumes.pp
@@ -6,5 +6,10 @@ plan powerstore::create_multiple_volumes(
   Integer[1,$volume_count].each |$i| {
     out::message("Creating volume vol_${i}")
     run_task('powerstore::volume_create', $targets, {name=>"vol_${i}", size=>1048576})
+    # apply($targets) {
+    #   powerstore_volume { "vol_${i}":
+    #     size => 1048576,
+    #   }
+    # }
   }
 }
