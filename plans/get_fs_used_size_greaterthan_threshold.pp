b1f48b8381397a9bee1e89dd09cc414a579c0672
= <dimitri@puppet.com>
2020-10-21 17:32:20 +0100

    fix 2

--- plans/get_fs_used_size_greaterthan_threshold.pp
+++ plans/get_fs_used_size_greaterthan_threshold.pp
@@ -6,8 +6,9 @@ plan powerstore::get_fs_used_size_greaterthan_threshold(
 ){
   $file_systems = run_task('powerstore::file_system_collection_query', $targets)[0].value
   $filtered_file_systems = $file_systems.filter | $k, $v | {
-    unless $v['size_total'] and $v['size_total'] > 0 { return false }
-    $v['size_used']*100.0 / $v['size_total'] > $threshold
+    if $v['size_total'] and $v['size_total'] > 0 {
+      $v['size_used']*100.0 / $v['size_total'] > $threshold
+    }
   }
   return $filtered_file_systems.map |$k,$v| { $k }
 }
