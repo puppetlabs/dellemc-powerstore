35709d924ef430ce0ed0b9c683fc00cb1bef2273
Cody Herriges <193064+ody@users.noreply.github.com>
2020-10-21 21:19:21 +0100

    More simplifications
    	Removes use of the filter() function, replaced by task native filtering

--- plans/policy.pp
+++ plans/policy.pp
@@ -46,23 +46,26 @@ plan powerstore::policy(
 
   # Task that interrogates the Powerstore devices to retrieve the post apply()
   # state of the snapshot rule resources we preveiously defined by filtering
-  # them from the list of all snapshot rules then extracts each rule's ID so
-  # it can be assigned to the device target using the set_var() function. We
-  # will use this device inventory variable in the next apply block
+  # them from the list of all snapshot rules, using an API native query string
+  # then extracts each rule's ID so it can be assigned to the device target
+  # using the set_var() function. We will use this device inventory variable in
+  # the next apply block
   #
-  # This first line is just running our task using an API collection query and
-  # returning a set of results that we'll interate over.
+  # These first few lines are running the task that uses an API collection query
+  # with a custom filter to return a set of results from multiple storage arrays
+  # that we'll interate over
   #   
-  run_task('powerstore::snapshot_rule_collection_query', $targets).each |$target_result| {
-    # This get_target() line fetches our device's inventory object and calls on
+  run_task('powerstore::snapshot_rule_collection_query', $targets, {
+    'query_string' => "select=name,id&name=in.(${snapshot_rules.join(',')})"
+  }).each |$target_result| {
+    # This get_target() line fetches our devices' inventory object and calls on
     # it the set_var() function to set the snapshot_rule_ids inventory variable
     #
     get_target($target_result.target).set_var('snapshot_rule_ids',
-      # Filter's the resources we got back from the collection query to only
-      # include the ones we care about and return their IDs so they are assigned
-      # to the correct device
+      # Use a map() function to transform and collect all the id properties from
+      # the resources retruned in our collection query in a single Array
       #
-      $target_result.value.filter |$rule| { $rule[0] in $snapshot_rules }.map |$match| { $match[1]['id'] }
+      $target_result.value.map |$rule| { $rule[1]['id'] }
     )
   }
 
@@ -76,17 +79,18 @@ plan powerstore::policy(
       }
     }
   }
- 
-  # Same strategy as with snapshot rules but a tad simpler because we only need
-  # to obtain the ID of our policy resource.
-  #   
-  run_task('powerstore::policy_collection_query', $targets).each |$target_result| {
-    get_target($target_result.target).set_var('policy_id',
-      $target_result.value.filter |$policy| { $policy[0] == 'default_org_snapshot_set' }.map |$match| { $match[1]['id'] }[0]
-    )
+
+  # Same strategy as with snapshot rules but simpler because we only need to
+  # obtain the id of a single known policy resource.
+  # 
+  run_task('powerstore::policy_collection_query', $targets, {
+    'query_string' => 'select=name,id&name=eq.default_org_snapshot_set'
+  }).each |$target_result| {
+    get_target($target_result.target).set_var('policy_id', $target_result.value['default_org_snapshot_set']['id'])
   }
 
-  # Update an existing volume with the policy that includes are snapshot rules,
+
+  # Update an existing volume with the policy that includes out snapshot rules,
   # set it to empty string if the plan is ran with 'ensure=absent'
   #
   apply($targets,  '_catch_errors' => true) {
@@ -101,11 +105,9 @@ plan powerstore::policy(
   # Same strategy as with snapshot rules but a tad simpler because we only need
   # to obtain the ID of our policy resource.
   #   
-  $volumes = Hash(run_task('powerstore::volume_collection_query', $targets).map |$target_result| {
-      $target_result.value.filter |$volume| { $volume[0] == $volume_name }.map |$match| {
-        [ "${target_result.target.name}_protection_policy_id", $match[1]['protection_policy_id'] ]
-      }[0]
-    })
+  $volumes = run_task('powerstore::volume_collection_query', $targets, {
+    'query_string' => "select=name,protection_policy_id&name=eq.${volume_name}"
+  })
 
   return $volumes
 }
--- plans/volume.pp
+++ plans/volume.pp
@@ -8,17 +8,22 @@ plan powerstore::volume(
 
   
   if $ensure == 'absent' {
-    $volumes = Hash(run_task('powerstore::volume_collection_query', $targets).map |$target_result| {
-        $target_result.value.filter |$volume| { $volume[0] == $volume_name }.map |$match| { [ "${target_result.target.name}_id", $match[1]['id'] ] }[0]
+    $volumes = Hash(run_task('powerstore::volume_collection_query', $targets, {
+      'query_string' => "select=id,name&name=eq.${volume_name}" }).map |$target_result| {
+        $target_result.value.map |$volume| { [ "${target_result.target.name}", { $volume[0] => $volume[1] } ] }[0]
     })
   
-    $results = get_targets($targets).each |$target| { run_task('powerstore::volume_delete', $target, { 'id' => $volumes["${target.name}_id"] } ) } 
+    $results = get_targets($targets).each |$target| {
+      run_task('powerstore::volume_delete', $target, {
+        'id' => $volumes["${target.name}"][$volume_name]['id']
+      })
+    } 
   } else {
     $results = run_task('powerstore::volume_create', $targets, {
       'name'        => $volume_name,
       'size'        => $size,
       'description' => $description
-    } )
+    })
   } 
 
   return $results
