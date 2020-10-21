7f32a97c30f239fb32d403f27777d2b3094e1b15
Dimitri Tischenko <dimitri@puppet.com>
2020-10-21 15:28:49 +0200

    better alignment

--- plans/volume_capacity.pp
+++ plans/volume_capacity.pp
@@ -3,7 +3,9 @@ plan powerstore::volume_capacity(
     Integer    $threshold,
 ){
   $volumes = run_task('powerstore::volume_collection_query', $targets)[0].value
-  $rows = $volumes.map |$k,$v| { [$k, $v['size'], String(format_bytes($v['size']), '%8s')] }.filter |$e| { $e[1] > $threshold }
+  $rows = $volumes.filter |$k,$v| { $v['size'] > $threshold }.map |$k,$v| {
+    [$k, String($v['size'],'%15s'), String(format_bytes($v['size']),'%10s')]
+  }
   $volumes_table = format::table({
     title => "List of volumes with capacity > ${threshold}",
     head => ['volume name', 'capacity', 'MB'].map |$field| { format::colorize($field, yellow) },
