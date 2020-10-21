41e8f8fd9c3b66daff59969ed69f325b91fd32ec
Dimitri Tischenko <dimitri@puppet.com>
2020-10-21 18:29:48 +0200

    more fixes

--- plans/get_fs_used_size_greaterthan_threshold.pp
+++ plans/get_fs_used_size_greaterthan_threshold.pp
@@ -6,7 +6,8 @@ plan powerstore::get_fs_used_size_greaterthan_threshold(
 ){
   $file_systems = run_task('powerstore::file_system_collection_query', $targets)[0].value
   $filtered_file_systems = $file_systems.filter | $k, $v | {
-    $v['size_total'] and ($v['size_used']*100.0) / $v['size_total'] > $threshold
+    unless $v['size_total'] and $v['size_total'] > 0 { return false }
+    $v['size_used']*100.0 / $v['size_total'] > $threshold
   }
   return $filtered_file_systems.map |$k,$v| { $k }
 }
