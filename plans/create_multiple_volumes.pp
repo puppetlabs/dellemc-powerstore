5a9de6a824b4200c9532247baf7e9f25f9dba23d
Dimitri Tischenko <dimitri@puppet.com>
2020-10-22 14:54:20 +0200

    using apply in create_multiple_volumes

--- plans/create_multiple_volumes.pp
+++ plans/create_multiple_volumes.pp
@@ -4,13 +4,13 @@ plan powerstore::create_multiple_volumes(
   TargetSpec $targets,
   Integer[1,100] $volume_count,
 ){
-  Integer[1,$volume_count].each |$i| {
-    out::message("Creating volume vol_${i}")
-    run_task('powerstore::volume_create', $targets, {name=>"vol_${i}", size=>1048576})
-    # apply($targets) {
-    #   powerstore_volume { "vol_${i}":
-    #     size => 1048576,
-    #   }
-    # }
+  # out::message("Creating volume vol_${i}")
+  # run_task('powerstore::volume_create', $targets, {name=>"vol_${i}", size=>1048576})
+  apply($targets) {
+    Integer[1,$volume_count].each |$i| {
+      powerstore_volume { "vol_${i}":
+        size => 1048576,
+      }
+    }
   }
 }
