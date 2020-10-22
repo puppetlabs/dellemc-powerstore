2c0fe692141e7febb517a03eff61efb5d0f80721
Dimitri Tischenko <dimitri@puppet.com>
2020-10-22 11:47:25 +0200

    WIP delete_multiple_volumes plan

--- plans/delete_multiple_volumes.pp
+++ plans/delete_multiple_volumes.pp
@@ -4,8 +4,21 @@ plan powerstore::delete_multiple_volumes(
   TargetSpec $targets,
   Integer[1,100] $volume_count,
 ){
+  $volumes = run_task("powerstore::volume_collection_query", $targets, {'query_string' => 'select=*,volume_groups'})[0].value
+  
+  $volumes_in_group = $volumes.filter | $k, $v | { !$v['volume_groups'].empty }
+
+  Integer[1,$volume_count].each |$i| {
+    $volume_name = "vol_${i}"
+    $volume_group = $volumes_in_group[$volume_name]
+    if $volume_group {
+      out::message("Removing volume ${volume_name} from volume group ${$volume_group}")
+      run_task('powerstore::volume_group_remove_members', $targets, {'id' => $volume_group['id'], 'volume_ids' => [$volumes[$volume_name]['id']]})
+    }
+  }
   apply($targets) {
     Integer[1,$volume_count].each |$i| {
+      $volume_name = "vol_${i}"
       notice("Deleting volume vol_${i}")
       powerstore_volume { "vol_${i}":
         ensure => absent,
