26f3afafd1c0d1635e38e74e9e0295a4c0e94e5f
Cody Herriges <193064+ody@users.noreply.github.com>
2020-10-23 13:12:31 -0700

    Add strings documentation
    	Adding "puppet strings" documentation headers to plans and clean
    	up some minor style issues along the way

--- plans/create_volume.pp
+++ plans/create_volume.pp
@@ -1,3 +1,23 @@
+# @summary
+#   A Bolt Plan that creates or deletes a new volume
+#
+# @see https://github.com/puppetlabs/dellemc-powerstore
+#
+# @example Creates a new volume named `db_backups`
+#   bolt plan run powerstore::create_volume --targets powerstore volume_name=db_backups
+#
+# @param volume_name
+#   A name for the new volume
+#
+# @param size
+#   Override the default size (25GB) of the new volume
+#
+# @param description
+#   A simple description to explain the purpose of the volume
+#
+# @param ensure
+#   If the volume should exist or be deleted
+#
 plan powerstore::create_volume(
   TargetSpec                $targets,
   String                    $volume_name,
@@ -6,14 +26,14 @@ plan powerstore::create_volume(
   Enum['present', 'absent'] $ensure       = 'present'
 ) {
 
-  
+
     $results = apply($targets,  '_catch_errors' => true) {
       powerstore_volume { $volume_name:
+        ensure      => $ensure,
         size        => $size,
         description => $description,
-        ensure      => $ensure,
       }
-    } 
+    }
 
   return $results
 }
