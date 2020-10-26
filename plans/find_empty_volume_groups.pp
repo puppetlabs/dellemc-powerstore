016f990b8269d394062ad20b8653f72f605c7adf
Dimitri Tischenko <dimitri@puppet.com>
2020-10-26 17:18:22 +0100

    clean up find_empty_volume_groups

--- plans/find_empty_volume_groups.pp
+++ plans/find_empty_volume_groups.pp
@@ -1,14 +1,13 @@
-# List volume groups without assigned volumes
+# Find empty volume groups - Puppet language plan example
 plan powerstore::find_empty_volume_groups(
   TargetSpec $targets,
-)
-{
-  # first, get volume groups with their names and assigned volumes
-  # use the query_string task parameter to override the query
+){
+  # Get volume groups with their names and assigned volumes
+  # Use the query_string task parameter to override the query
   $volume_groups = run_task('powerstore::volume_group_collection_query', $targets, {query_string=>'select=name,volumes'})[0].value
 
-  # now, filter the volume_groups to only those without volumes and take their names
-  $empty_volume_groups = $volume_groups.filter |$k, $v| { $v['volumes'].empty }.map |$k, $v| { $k }
+  # Filter volume_groups to only those without volumes and take their names
+  $empty_volume_groups = $volume_groups.filter |$k, $v| { $v['volumes'].empty }.map |$k, $v| { $v['name'] }
 
   out::message("List of empty volume groups:\n${empty_volume_groups}")
 }
