89485f827dc2a0cf0ff7eda75456446c3b57c5e2
Dimitri Tischenko <dimitri@puppet.com>
2020-10-27 12:35:20 +0100

    using first

--- plans/capacity_volumes.pp
+++ plans/capacity_volumes.pp
@@ -1,17 +1,20 @@
 # list volumes with more than given capacity
-# @param threshold Volume capacity needed (in bytes)
+# @param threshold Volume capacity needed (in bytes/MB/GB/TB)
 plan powerstore::capacity_volumes(
     TargetSpec              $targets,
     Variant[Numeric,String] $threshold,
 ){
-  $volumes = run_task('powerstore::volume_collection_query', $targets)[0].value
+  $volumes = run_task('powerstore::volume_collection_query', $targets).first.value
+
   $rows = $volumes.filter |$k,$v| { $v['size'] > to_bytes($threshold) }.map |$k,$v| {
     [$k, String($v['size'],'%15s'), String(format_bytes($v['size']),'%10s')]
   }
+
   $volumes_table = format::table({
     title => "List of volumes with capacity > ${threshold}",
     head => ['volume name', 'capacity', 'MB'].map |$field| { format::colorize($field, yellow) },
     rows => $rows
-    })
+  })
+
   out::message($volumes_table)
 }
